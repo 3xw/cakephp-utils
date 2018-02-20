b85989eb4393fa27c609a15261d25b97dbc1c370
Wallef Antoine <antoine.wallef@gmail.com>
2018-02-20 10:35:33 +0100

    IsUniqueTransaltionRule

---
+++ src/Model/Rule/IsUniqueTranslationRule.php
@@ -0,0 +1,50 @@
+<?php
+namespace Trois\Utils\Model\Rule;
+
+use Cake\Datasource\EntityInterface;
+
+class IsUniqueTranslationRule
+{
+  protected $_fields;
+  protected $_options;
+
+  public function __construct(array $fields, array $options = [])
+  {
+    $this->_fields = $fields;
+    $this->_options = $options;
+  }
+
+  public function __invoke(EntityInterface $entity, array $options)
+  {
+    // check if transaltion
+    $behavior = $options['repository']->behaviors()->get('Translate');
+    if(!$behavior) return true;
+
+    $association = $options['repository']->association($behavior->getConfig('translationTable'));
+
+    $result = true;
+    foreach($this->_fields as $field)
+    {
+      foreach ($entity->get('_translations') as $locale => $translation)
+      {
+        $conditions = [
+          $association->aliasField('field') => $field,
+          $association->aliasField('locale') => $locale,
+          $association->aliasField('content') => $translation->get($field)
+        ];
+        if ($entity->isNew() === false) $conditions[$association->aliasField('foreign_key').' IS NOT'] = $entity->get('id');
+
+        if ($association->exists($conditions))
+        {
+          $translation->setErrors([
+            $field => [
+              'uniqueTranslation' => __d('cake', 'This value is already in use')
+            ]
+          ]);
+          $result = false;
+        }
+      }
+    }
+    return $result;
+  }
+}
