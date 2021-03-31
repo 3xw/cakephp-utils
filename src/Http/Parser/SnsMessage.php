dc4a0a9a292f6247e94e75bb1b75a2ae9a9c17e1
jeanvier <damien.grossfeld@gmail.com>
2021-03-31 14:37:37 +0200

    Sns

---
+++ src/Http/Parser/SnsMessage.php
@@ -0,0 +1,33 @@
+<?php
+namespace Trois\Utils\Http\Parser;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+use Aws\Sns\Message;
+use Aws\Sns\MessageValidator;
+use Aws\Sns\Exception\InvalidSnsMessageException;
+
+use Cake\Http\Exception\BadRequestException;
+use Cake\Datasource\EntityInterface;
+
+use Trois\Utils\Model\Entity\SnsMessage as Entity;
+
+class SnsMessage
+{
+  public static function parse(
+    ServerRequestInterface $request,
+    array $options = []
+  ): EntityInterface
+  {
+    try {
+      $message = Message::fromPsrRequest($request);
+      $validator = new MessageValidator();
+      $validator->validate($message);
+    } catch (InvalidSnsMessageException $e) {
+      throw new BadRequestException($e->getMessage());
+    }
+
+    // create entity
+    return new Entity($message->toArray(), $options);
+  }
+}
